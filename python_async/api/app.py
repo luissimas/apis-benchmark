925ffb82c12c01aca63214a5d8303f7ce9f1a4d6
Luís Simas <luissimas@protonmail.com>
2023-08-24 21:15:36 -0300

    fix: python async pg pool

--- python_async/api/app.py
+++ python_async/api/app.py
@@ -1,4 +1,3 @@
-import asyncio
 from dataclasses import dataclass
 from datetime import date, datetime
 from typing import List, Optional
@@ -6,13 +5,9 @@ from uuid import UUID, uuid4
 
 import asyncpg
 from api.config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
-from fastapi import Depends, FastAPI
+from fastapi import FastAPI
 
 dsn = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-
-
-async def init_db_pool():
-    return await asyncpg.create_pool(dsn=dsn, min_size=1, max_size=100)
 
 
 @dataclass
@@ -48,9 +43,14 @@ def make_movies() -> List[Movie]:
 app = FastAPI()
 
 
+@app.on_event("startup")
+async def startup():
+    app.state.db_pool = await asyncpg.create_pool(dsn=dsn, min_size=1, max_size=100)
+
+
 @app.get("/db")
-async def get_db(pool: asyncpg.Pool = Depends(init_db_pool)):
-    async with pool.acquire() as conn:
+async def get_db():
+    async with app.state.db_pool.acquire() as conn:
         query = "SELECT * FROM movies LIMIT 20"
         rows = await conn.fetch(query)
         movies = [Movie(**row) for row in rows]
